bf819ada28321b45e64925a030b5a20553daa435
RafaelCasaSanta <rafael.casasanta@edu.unifil.br>
2022-09-21 15:15:51 -0300

    modal de cadastro de aluno

--- resources/views/student-page.blade.php
+++ resources/views/student-page.blade.php
@@ -7,43 +7,58 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="card">
-            <div class=" header-card card-body card-background">Gerenciamento de Projetos</div>
+            <div class=" header-card card-body card-background">Estudantes</div>
         </div>
     </x-slot>
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
+    <div class="container mt-5">
+        <button type="button" class="btn btn-primary centralizar" data-bs-toggle="modal" data-bs-target="#myModal">Cadastrar Aluno</button>
+        <div class="modal" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Cadastro de Estudantes</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="mb-3">
+                                <label class="form-label required">Nome Completo</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label required">Email Institucional</label>
+                                <input type="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label required">Matricula do Aluno</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label required">Curso Matriculado</label>
+                                <select class="form-select form-control" aria-label="Default select example" name='courseYear'>
+                                    <option selected>Selecionar curso</option>
+                                    <option value="1">Analise e Desenvolvimento de Sistemas</option>
+                                    <option value="2">Ciência da Computação</option>
+                                    <option value="3">Engenharia de Software</option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Registrar</button>
+                        <button type="submit" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img class="carousel-image"
-                    src="http://portal.filadelfia.br:7778/PortalLondrinenseUniFil/assets/images/Portais/Aluno_Graduacao.jpg"
-                    class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
 
 </x-app-layout>
 
@@ -68,6 +83,11 @@
     height: 100%;
 }
 
+.centralizar {
+    display: flex;
+    justify-content: end !important;
+}
+
 .header-card {
     color: #ffff;
     background-color: #fb923c;
@@ -75,4 +95,16 @@
     justify-content: center
 }
 
+.modal-header {
+            background: #F7941E;
+            color: #fff;
+        }
+
+        .required:after {
+            content: "*";
+            color: red;
+        }
+
 </style>
+
+
