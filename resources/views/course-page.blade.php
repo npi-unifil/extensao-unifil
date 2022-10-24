7f76a51a01d44558b01e460baf9e444c44a747be
RafaelCasaSanta <rafael.casasanta@edu.unifil.br>
2022-10-24 15:33:44 -0300

    Mudança visual da tabela de cursos

--- resources/views/course-page.blade.php
+++ resources/views/course-page.blade.php
@@ -33,21 +33,22 @@
                     <td scope="row">
                         <div class="flex d-flex gap-2" style="justify-content: center;">
 
-                            <div class="container action">
-                                {{-- <button type="button"  class="btn  centralizar">
-                                    <a href="/coursePage/studentEdit/{{$student->id}}">
+                            {{-- <div class="container action">
+                                <a href="/coursePage/studentEdit/{{$student->id}}">
+                                <button type="button"  class="btn  centralizar">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16"
                                     height="16" fill="currentColor"
                                     class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                     <path
                                         d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                                 </svg>
-                                </button> --}}
+                                </button>
+                            </div> --}}
 
 
                             {{-- Botão de Editar Cadastro de Alunos --}}
-                        {{-- <a href='/coursePage/{{ $student->id }}'>
-                            <button class="btn edit-button">
+                        <a href='/coursePage/studentEdit/{{$student->id}}'>
+                            <button class="btn  centralizar edit-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16"
                                     height="16" fill="currentColor"
                                     class="bi bi-pencil-fill" viewBox="0 0 16 16">
@@ -55,14 +56,10 @@
                                         d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                                 </svg>
                             </button>
-                        </a> --}}
-
-
-
-
-
+                        </a>
 
                         {{-- Botão de Modal de Exlusão novo --}}
+                        {{-- <div class="container action"> --}}
                             <button type="button" class="btn excluir" data-bs-toggle="modal" data-bs-target="#delete-students">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16"
                                     height="16" fill="currentColor"
@@ -82,7 +79,7 @@
                                         </div>
                                         <div class="modal-body">
                                             <b style="display:flex; justify-content:center;">Deseja Realmente remover o aluno ?</b>
-                                            <form action="/coursePage/{{ $student->id }}" method="POST">
+                                            <form action="/studentDelete/{{ $student->id }}" method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 </div>
--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -10,7 +10,9 @@
             <div class=" header-card card-body card-background">Cursos</div>
         </div>
 
-        <div class=" body-card card-body">
+
+
+        {{-- <div class=" body-card card-body">
         @foreach ($courses as $course)
 
         <div class="list-group list-custom">
@@ -36,11 +38,50 @@
         </div>
 
 
-        @endforeach
+        @endforeach --}}
     </x-slot>
 
     @section('conteudo')
 
+    <div class="tabela" style="width: 100%;">
+    <table class="table table-dark" style="width: 80%">
+        <thead>
+        <tr>
+            <th scope="col">Cursos</th>
+            <th scope="col">Ação</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($courses as $course)
+        <tr>
+            {{-- <div class=" body-card card-body"> --}}
+            <div class="list-group list-custom">
+                <td>
+                    <a class="list-group-item" href="{{route('courseId', $course->id)}}">{{$course['courseName']}}</a>
+                </td>
+                <td>
+                    <form action="/coursePage/{{ $course->id }}" method="POST"
+                        onsubmit="return confirm('Tem certeza que deseja remover {{ $course->nome }} ?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class='btn btn-danger'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                <path
+                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                            </svg>
+                        </button>
+                    </form>
+                </td>
+            </div>
+        </div>
+    </tr>
+        </tbody>
+    @endforeach
+    </table>
+</div>
 
     <div class="newCourse">
         <a type="button" class="button btn " href='courseForm'>Criar novo curso</a>
@@ -118,6 +159,11 @@
     justify-content: space-between;
     margin-top: 3px;
     text-decoration-color: #374151;
+}
+
+.tabela {
+    display:flex;
+    justify-content: center;
 }
 
 </style>
